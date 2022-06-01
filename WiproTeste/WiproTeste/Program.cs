1a15578349e3636065882196e1ac22d6473251ba
Nícolas BLS <nycolabls@gmail.com>
2022-06-01 00:24:57 -0300

    Configuração Swagger

--- WiproTeste/WiproTeste/Program.cs
+++ WiproTeste/WiproTeste/Program.cs
@@ -40,7 +40,7 @@ else
     app.UseSwaggerUI(c =>
     {
         c.RoutePrefix = string.Empty;
-        c.SwaggerEndpoint($"{contentRoot}swagger/v1/swagger.json", "My API V1");
+        c.SwaggerEndpoint($"swagger/v1/swagger.json", "My API V1");
     });
 }
 
