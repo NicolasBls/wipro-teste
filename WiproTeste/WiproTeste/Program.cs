8a756b56a00c3f34fb4ddeb38c5000aea16632bd
Nícolas BLS <nycolabls@gmail.com>
2022-06-01 00:32:03 -0300

    Configuração Swagger

--- WiproTeste/WiproTeste/Program.cs
+++ WiproTeste/WiproTeste/Program.cs
@@ -23,26 +23,21 @@ builder.Services.AddAutoMapper(typeof(MapperProfile));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.  
+app.UseSwagger();
+
 if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
     app.UseSwaggerUI(c =>
     {
         c.RoutePrefix = string.Empty;
         c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
     });
-}
 else
-{
-    var contentRoot = builder.Configuration.GetValue<string>(WebHostDefaults.ContentRootKey);
-
-    app.UseSwagger();
     app.UseSwaggerUI(c =>
     {
         c.RoutePrefix = string.Empty;
         c.SwaggerEndpoint($"swagger/v1/swagger.json", "My API V1");
     });
-}
+
 
 app.UseHttpsRedirection();
 
